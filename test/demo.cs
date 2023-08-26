6bcab34fed91516370b3597a4cfeb5e3897f910a
snsinahub <snsinahub@github.com>
2023-08-26 19:58:06 -0400

    add

--- test/demo.cs
+++ test/demo.cs
@@ -82,28 +82,28 @@ namespace ws.SeleniumTests
 
         
 
-        [TestMethod]
-        public void TestLink4()
-        {
-            // Chrome Driver was manually downloaded from https://sites.google.com/a/chromium.org/chromedriver/downloads
-            // parameter "." will instruct to look for the chromedriver.exe in the current folder (bin/debug/...)
-            using (var driver = GetDriver())
-            {
-                test = extent.CreateTest(TestContext.TestName);
-                // extent.LogInfo("TestLink2");
-                //Navigate to DotNet website
-                driver.Navigate().GoToUrl((string)TestContext.Properties["webAppUrl"]);
-                //Click the Get Started button
-                // test url link if correct after click find element by id
+        // [TestMethod]
+        // public void TestLink4()
+        // {
+        //     // Chrome Driver was manually downloaded from https://sites.google.com/a/chromium.org/chromedriver/downloads
+        //     // parameter "." will instruct to look for the chromedriver.exe in the current folder (bin/debug/...)
+        //     using (var driver = GetDriver())
+        //     {
+        //         test = extent.CreateTest(TestContext.TestName);
+        //         // extent.LogInfo("TestLink2");
+        //         //Navigate to DotNet website
+        //         driver.Navigate().GoToUrl((string)TestContext.Properties["webAppUrl"]);
+        //         //Click the Get Started button
+        //         // test url link if correct after click find element by id
 
 
-                driver.FindElement(By.Id("redirect")).Click();                
+        //         driver.FindElement(By.Id("redirect")).Click();                
                 
-                Assert.AreEqual(driver.Url, "https://dotnet.microsoft.com/");
+        //         Assert.AreEqual(driver.Url, "https://dotnet.microsoft.com/");
     
                 
-            }
-        }
+        //     }
+        // }
 
         [TestMethod]
         public void TestLink5()
